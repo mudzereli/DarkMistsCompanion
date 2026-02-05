27ef312a38d82f3577f3db782e6524d450719aad
mudzereli <mudz@charter.net>
2026-02-05 16:19:11 -0600

    Create mapwindow.lua

---
+++ ui/mapwindow.lua
@@ -0,0 +1,34 @@
+MiniMapContainer =
+  Adjustable.Container:new({
+    name = "MiniMapContainer",
+
+    x = "70%",
+    y = "0%",
+    width = "30%",
+    height = "50%",
+
+    titleText = "Mini Map",
+    titleTxtColor = "white",
+    padding = 10,
+    adjLabelstyle = [[
+      background-color: #111111;
+      border: 2px solid #666666;
+    ]],
+    
+    lockStyle = "border",
+    locked = false,
+    autoSave = true,
+    autoLoad = true,
+  })
+
+MiniMap =
+  Geyser.Mapper:new({
+    name = "MiniMap",
+    x = 0, y = 0,
+    width = "100%", height = "100%"
+  }, MiniMapContainer)
+
+  tempTimer(2,function()
+    MiniMapContainer:show()
+    MiniMapContainer:raiseAll()
+  end)
