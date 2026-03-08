9b1ca3ade79f0fdf91c8a1a8dec18336ec52a5dd
mudzereli <mudz@charter.net>
2026-03-08 10:36:32 -0500

    Update tabwindow.lua

--- ui/tabwindow.lua
+++ ui/tabwindow.lua
@@ -1,3 +1,57 @@
+local isLight = Darkmists.GlobalSettings.lightMode
+
+local inactiveStyle = isLight and [[
+  QLabel {
+    background-color: rgb(245,245,250);
+    border: 1px solid rgb(220,220,235);
+    margin-left: 1px;
+    margin-right: 1px;
+    padding: 6px;
+    qproperty-alignment: 'AlignCenter';
+  }
+  QLabel:hover {
+    background-color: rgb(235,225,255);
+    border: 1px solid rgb(200,180,255);
+  }
+]]
+or [[
+  QLabel {
+    background-color: rgba(255,255,255,5%);
+    margin-left: 1px;
+    margin-right: 1px;
+    padding: 6px;
+    qproperty-alignment: 'AlignCenter';
+  }
+  QLabel:hover {
+    background-color: rgba(180,160,255,28%);
+  }
+]]
+
+local activeStyle = isLight and [[
+  QLabel {
+    background-color: rgb(170,140,255);
+    color: white;
+    border-top: 3px solid rgb(48, 51, 107);   /* Dark Mists deep purple */
+    margin-left: 1px;
+    margin-right: 1px;
+    padding: 6px;
+    font-weight: bold;
+    qproperty-alignment: 'AlignCenter';
+  }
+]]
+or [[
+  QLabel {
+    background-color: rgb(48, 51, 107);   /* Dark Mists deep purple */
+    color: white;
+    border-top: 3px solid rgb(170,140,255);
+    margin-left: 1px;
+    margin-right: 1px;
+    padding: 6px;
+    font-weight: bold;
+    qproperty-alignment: 'AlignCenter';
+  }
+]]
+
 DMTabs = Adjustable.TabWindow:new({
   name = "DMTabs",
 
@@ -8,34 +62,18 @@ DMTabs = Adjustable.TabWindow:new({
 
   tabs = {"Chat","Affects","Who","Player"},
 
-  color1 = Darkmists.getDefaultBackgroundColor(),
-  color2 = Darkmists.getDefaultBackgroundColor(),
+  color1 = Darkmists.getDefaultTextColor(),
+  color2 = Darkmists.getDefaultTextColor(),
   tabTxtColor = Darkmists.getDefaultTextColor(),
 
-  -- INACTIVE TABS
-  inactiveTabStyle = [[
-    QLabel {
-      background-color: rgba(255,255,255,5%);
-      margin-left: 1px;
-      margin-right: 1px;
-      padding: 6px;
-      qproperty-alignment: 'AlignCenter';
-    }
-    QLabel:hover {
-      background-color: rgba(180,160,255,12%);
-    }
+  inactiveTabStyle = inactiveStyle,
+  activeTabStyle   = activeStyle,
+  footerStyle = [[
+    background-color: ]] .. Darkmists.getDefaultBackgroundColor() .. [[;
   ]],
-
-  -- ACTIVE TAB (Dark Mists purple accent)
-  activeTabStyle = [[
-    QLabel {
-      background-color: rgba(180,160,255,20%);
-      border-top: 2px solid rgb(170,140,255);
-      margin-left: 1px;
-      margin-right: 1px;
-      padding: 6px;
-      font-weight: bold;
-      qproperty-alignment: 'AlignCenter';
-    }
-  ]],
+  centerStyle = [[
+    background-color: ]] .. Darkmists.getDefaultBackgroundColor() .. [[;
+    border-radius: 6px;
+    margin: 4px;
+  ]]
 })
