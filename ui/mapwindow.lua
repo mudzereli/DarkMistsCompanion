dcc12a7e24f4c4db9e3f1f1294a2f6dd509aed4e
mudzereli <mudz@charter.net>
2026-03-09 08:49:44 -0500

    Update mapwindow.lua
    refactor

--- ui/mapwindow.lua
+++ ui/mapwindow.lua
@@ -7,117 +7,114 @@ DarkMistsMiniMap.minimap = nil
 
 local dock = Darkmists.getSmartDockGeometry()
 
-DarkMistsMiniMap.create = function()
-  -- create main container
+-- -------------------------------------------------------------------
+-- UI Creation
+-- -------------------------------------------------------------------
+function DarkMistsMiniMap.create()
   DarkMistsMiniMap.container = Adjustable.Container:new({
     name = "MiniMapContainer",
-
     x = dock.x,
     width = dock.width,
     y = "0%",
     height = "50%",
-
     titleText = "Mini Map",
     titleTxtColor = Darkmists.getDefaultTextColor(),
     padding = 10,
     adjLabelstyle = Darkmists.getDefaultAdjLabelstyle(),
-    
     lockStyle = "border",
     locked = false,
     autoSave = true,
     autoLoad = true,
   })
 
-  -- create header inside of container
-  do
-    DarkMistsMiniMap.header = Geyser.Label:new({
-      name = "MiniMapHeader",
-      x = "0%", y = "0%",
-      width = "100%", height = "10%"
-    }, DarkMistsMiniMap.container)
-    -- apply stylesheet to header
-    DarkMistsMiniMap.header:setStyleSheet([[
-      padding: 0px;
-      margin: 0px;
-      font-weight: 900;
-      qproperty-alignment: AlignCenter;
-    ]])
-    DarkMistsMiniMap.header:setFont(Darkmists.GlobalSettings.fontName)
-    DarkMistsMiniMap.header:setFontSize(Darkmists.GlobalSettings.fontSize)
-  end
+  -- Header
+  local header = Geyser.Label:new({
+    name = "MiniMapHeader",
+    x = "0%", y = "0%",
+    width = "100%", height = "10%"
+  }, DarkMistsMiniMap.container)
 
-  -- create minimap container inside
+  header:setStyleSheet([[
+    padding: 0px;
+    margin: 0px;
+    font-weight: 900;
+    qproperty-alignment: AlignCenter;
+  ]])
+  header:setFont(Darkmists.GlobalSettings.fontName)
+  header:setFontSize(Darkmists.GlobalSettings.fontSize)
+
+  DarkMistsMiniMap.header = header
+
+  -- Mapper
   DarkMistsMiniMap.minimap = Geyser.Mapper:new({
-      name = "MiniMap",
-      x = "0%", y = "10%",
-      width = "100%", height = "90%"
-    }, DarkMistsMiniMap.container)
+    name = "MiniMap",
+    x = "0%", y = "10%",
+    width = "100%", height = "90%"
+  }, DarkMistsMiniMap.container)
 
   Darkmists.Log("MiniMapContainer","Container Created!")
 end
 
-DarkMistsMiniMap.update = function()
-  local room = {
-    name = nil,
-    id = nil,
-    area = nil
-  }
-  -- try to use map selection first
-  local s = getMapSelection()
-  if s then
-    room.id = s.center
-    if room.id then
-      room.name = getRoomName(room.id)
-    end
-    if room.name then
-      room.area = getRoomAreaName(getRoomArea(room.id))
-    end
-  end
-  -- if that doesn't work, try to use current room
-  if not (room.name and room.id and room.area) then
-    -- try to use current room info
-    if map.currentRoom then
-      room.name = map.currentName
-      room.id = map.currentRoom
-      room.area = getRoomAreaName(map.currentArea)
+-- -------------------------------------------------------------------
+-- Update Display
+-- -------------------------------------------------------------------
+function DarkMistsMiniMap.update()
+  local name, id, area
+
+  -- Prefer selected room
+  local selection = getMapSelection()
+  if selection then
+    id = selection.center
+    if id then
+      name = getRoomName(id)
+      if name then
+        area = getRoomAreaName(getRoomArea(id))
+      end
     end
   end
-  -- if anything doesn't work, set it to unknown
-  if not (room.name) then
-    room.name = "unknown"
+
+  -- Fallback to current room
+  if not (name and id and area) and map.currentRoom then
+    name = map.currentName
+    id = map.currentRoom
+    area = getRoomAreaName(map.currentArea)
   end
-  if not (room.id) then
-    room.id = "?"
-  end
-  if not (room.area) then
-    room.area = "unknown"
-  end
-  -- emulate mapper display and show it
-  local disp = "%s / %s (%s)"
-  disp = disp:format(room.name,tostring(room.id),room.area)
+
+  -- Defaults
+  name = name or "unknown"
+  id = id or "?"
+  area = area or "unknown"
+
+  local disp = ("%s / %s (%s)"):format(name, tostring(id), area)
   DarkMistsMiniMap.header:clear()
   DarkMistsMiniMap.header:echo(disp)
 end
 
-DarkMistsMiniMap.registerEvents = function()
-  -- kill old event handlers
-  for _, v in ipairs(DARKMISTS_MINIMAP_EVENT_HANDLERS) do
-    killAnonymousEventHandler(v)
-    Darkmists.Log("MiniMapContainer",("Killing Event Handler #%d"):format(v))
+-- -------------------------------------------------------------------
+-- Event Management
+-- -------------------------------------------------------------------
+function DarkMistsMiniMap.registerEvents()
+  -- Remove existing handlers
+  for _, id in ipairs(DARKMISTS_MINIMAP_EVENT_HANDLERS) do
+    killAnonymousEventHandler(id)
+    Darkmists.Log("MiniMapContainer", ("Killing Event Handler #%d"):format(id))
   end
   DARKMISTS_MINIMAP_EVENT_HANDLERS = {}
 
-  -- update on map window mouse press event
+  -- Register new handlers
   table.insert(DARKMISTS_MINIMAP_EVENT_HANDLERS,
     registerAnonymousEventHandler("sysMapWindowMousePressEvent", DarkMistsMiniMap.update))
 
-  -- update on room exit updated
   table.insert(DARKMISTS_MINIMAP_EVENT_HANDLERS,
     registerAnonymousEventHandler("dmapi.world.prompt", DarkMistsMiniMap.update))
+
   Darkmists.Log("MiniMapContainer","Events Registered!")
 end
 
-tempTimer(1,function()
+-- -------------------------------------------------------------------
+-- Initialization
+-- -------------------------------------------------------------------
+tempTimer(1, function()
   DarkMistsMiniMap.create()
   DarkMistsMiniMap.registerEvents()
   DarkMistsMiniMap.update()
